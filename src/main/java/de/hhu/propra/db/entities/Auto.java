226b20cb5d3d35ad852b1d1c4239428d7f51bd61
Burak Martin <burak.martin@mail.de>
2018-11-15 15:11:54 +0100

    Kim stinkt nach Apfel

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,21 @@
 package de.hhu.propra.db.entities;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Entity
 public class Auto{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "marke")
+    private String marke;
+    @Column(name = "farbe")
+    private String farbe;
+    @Column(name = "hubraum")
+    private double hubraum;
 }
